2e4b457b8c9a3eba17c14b7c8abfd5c546f82a34
Metin Zühre <79897762+m3tozz@users.noreply.github.com>
2024-12-24 10:27:02 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,9 +1,10 @@
 	clear
 	echo -e "\033[0;Installing...\033[1;36m"
-    cd src/boot
-    make
-    cd ..
-    cd main
-    make
-	clear
+   	cd src/boot
+    	make
+   	cd ..
+    	cd main
+   	make
+    	cd .. && cd ..
+     	clear
 	echo -e "\033[31m Installed!\033[0m"
